9c61c9642995bbd0c826499413fde65a0aa49619
charchalis <charchalis97@gmail.com>
2024-12-28 20:04:24 +0000

    size menu

--- src/game.pl
+++ src/game.pl
@@ -2,7 +2,8 @@
 
 play :-
     welcome,
-    DefaultConfig = game_config(human_vs_computer, easy),
+    DefaultConfig = game_config(human_vs_computer, easy, small),
+    write('popo'),
     main_menu(DefaultConfig).
 
 welcome :-
@@ -19,12 +20,13 @@ reload :-
 
 
 main_menu(GameConfig) :-
-    GameConfig = game_config(GameType, Difficulty),
-    write('1. start ('), write(GameType), write(' '), write(Difficulty), write(')'), nl,
+    GameConfig = game_config(GameType, Difficulty, Size),
+    write('1. start ( '), write(GameType), write(' | '), write(Difficulty), write(' | '), write(Size), write(' board )'), nl,
     write('2. game type'), nl,
     write('3. difficulty'), nl,
-    write('4. leave'), nl, nl,
-    write('Choose an option (1-4): '),
+    write('4. board size'), nl,
+    write('5. leave'), nl, nl,
+    write('Choose an option (1-5): '),
     read(Option), nl,
     handle_main_menu(Option, GameConfig).
 
@@ -39,11 +41,13 @@ handle_main_menu(3, GameConfig):-
     difficulty_menu(GameConfig).
 
 handle_main_menu(4, GameConfig):-
+    size_menu(GameConfig).
+handle_main_menu(5, GameConfig):-
     write('Bye bye.'), nl,
     halt.
 
 
-game_type_menu(game_config(_, Difficulty)) :-
+game_type_menu(game_config(_, Difficulty, _)) :-
     write('1. Human vs Human (H/H)'), nl,
     write('2. Human vs Computer (H/PC)'), nl,
     write('3. Computer vs Human (PC/H)'), nl,
@@ -51,7 +55,7 @@ game_type_menu(game_config(_, Difficulty)) :-
     write('Choose an option (1-4): '),
     read(Option), nl,
     handle_game_type_menu(Option, GameType),
-    main_menu(game_config(GameType, Difficulty)).
+    main_menu(game_config(GameType, Difficulty, Size)).
 
 handle_game_type_menu(1, human_vs_human).
 handle_game_type_menu(2, human_vs_computer).
@@ -59,18 +63,29 @@ handle_game_type_menu(3, computer_vs_human).
 handle_game_type_menu(4, computer_vs_computer).
 
 
-difficulty_menu(game_config(GameType, _)) :-
+difficulty_menu(game_config(GameType, _, Size)) :-
     write('1. easy'), nl,
     write('2. medium'), nl,
     write('3. hard'), nl,
     write('Choose an option (1-3): '),
     read(Option), nl,
     handle_difficulty_menu(Option, Difficulty),
-    main_menu(game_config(GameType, Difficulty)).
+    main_menu(game_config(GameType, Difficulty, Size)).
 
 handle_difficulty_menu(1, easy).
 handle_difficulty_menu(2, medium).
 handle_difficulty_menu(3, hard).
+
+size_menu(game_config(GameType, Difficulty, _)) :-
+    write('1. small board\t(6x6)'), nl,
+    write('2. big board\t(8x8)'), nl,
+    write('Choose an option (1-2): '),
+    read(Option), nl,
+    handle_size_menu(Option, Size),
+    main_menu(game_config(GameType, Difficulty, Size)).
+
+handle_size_menu(1, small).
+handle_size_menu(2, big).
 
 initial_state(GameConfig, GameState).
 
