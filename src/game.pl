59b2302850ecb18c698b4fd956ba67608cb1607d
charchalis <charchalis97@gmail.com>
2024-12-28 13:01:03 +0000

    main functions

--- src/game.pl
+++ src/game.pl
@@ -1,2 +1,15 @@
 % Anaash Game in SICStus Prolog
 
+initial_state(GameConfig, GameState).
+
+display_game(GameState).
+
+move(GameState, Move, NewGameState).
+
+valid_moves(GameState, Winner).
+
+game_over(GameState, Winner).
+
+value(GameState, Player, Value).
+
+choose_move(GameState, Level, Move).
